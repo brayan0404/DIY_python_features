05de8a382c19f115db2c9ee3c91f83e66864c2f0
Brayan Padilla <85806045+brayan0404@users.noreply.github.com>
2026-01-20 01:34:23 -0600

    Update lefPad.py

--- lefPad.py
+++ lefPad.py
@@ -5,11 +5,13 @@ def lefPad(string: str, ch: str, size: int) -> str | int:
 
     The js version of this function almost break the internet
     """
-    if (not isinstance(string, str) or not isinstance(ch, str) or not isinstance(size, int) or len(ch) != 1):
+    if (not isinstance(string, str) or not isinstance(ch, str) or 
+        not isinstance(size, int) or len(ch) != 1):
         return -1
     return (ch * (size - 1)) + string
 
 assert lefPad("B", "*", 3) == "**B" 
 assert lefPad("B", "**", 3) == -1
 assert lefPad("B", "*", 1) == "B"
+
 assert lefPad(1, "*", 1) == -1
