b68b6e62bedac921f22ca222290c2d2048603a46
Brayan Padilla <85806045+brayan0404@users.noreply.github.com>
2026-01-20 01:33:04 -0600

    Add files via upload
    I am so surprise that devs actually import a library that just do this kind of computation, of course i am talking about js devs cause py devs don't do that, do they?

---
+++ lefPad.py
@@ -0,0 +1,15 @@
+def lefPad(string: str, ch: str, size: int) -> str | int:
+    """
+    -----This is a joke-----
+    Version of rjust of python
+
+    The js version of this function almost break the internet
+    """
+    if (not isinstance(string, str) or not isinstance(ch, str) or not isinstance(size, int) or len(ch) != 1):
+        return -1
+    return (ch * (size - 1)) + string
+
+assert lefPad("B", "*", 3) == "**B" 
+assert lefPad("B", "**", 3) == -1
+assert lefPad("B", "*", 1) == "B"
+assert lefPad(1, "*", 1) == -1
